d1702963a8cb69685ffc9ac8832eb3c5dbd67ba1
Ian Ramos <juventuddisponible@gmail.com>
2021-03-01 09:27:36 -0600

    finished game for levels 1 and 2, start and end scenes

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class StartMenu : MonoBehaviour
 {
     [SerializeField] GameObject textBox;
+    [SerializeField] AudioSource reload;
 
     // Start is called before the first frame update
     void Start()
     {
         textBox.SetActive(false);
 
-        Invoke("DisplayText", 1f);
+        Invoke("DisplayText", 2f);
     }
     private void DisplayText()
     {
         textBox.SetActive(true);
+        reload.Play();
     }
 
 }
